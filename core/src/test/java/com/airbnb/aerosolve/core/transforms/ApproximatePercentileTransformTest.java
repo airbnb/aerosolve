4883f3c973338c4fc9e210c10a7fbc55c3ebddd8
Hector Yee <hector.yee@airbnb.com>
2015-05-14 15:47:42 -0700

    Change from piecewise linear to linear for approximate percentile transform

--- core/src/test/java/com/airbnb/aerosolve/core/transforms/ApproximatePercentileTransformTest.java
+++ core/src/test/java/com/airbnb/aerosolve/core/transforms/ApproximatePercentileTransformTest.java
@@ -18,7 +18,7 @@ import static org.junit.Assert.assertTrue;
 public class ApproximatePercentileTransformTest {
   private static final Logger log = LoggerFactory.getLogger(ApproximatePercentileTransformTest.class);
 
-  public FeatureVector makeFeatureVector(double val) {
+  public FeatureVector makeFeatureVector(double low, double high, double val) {
     Map<String, Set<String>> stringFeatures = new HashMap<>();
     Map<String, Map<String, Double>> floatFeatures = new HashMap<>();
 
@@ -28,9 +28,8 @@ public class ApproximatePercentileTransformTest {
     stringFeatures.put("strFeature1", list);
 
     Map<String, Double> map = new HashMap<>();
-    map.put("10th", 10.0);
-    map.put("50th", 20.0);
-    map.put("90th", 100.0);
+    map.put("10th", low);
+    map.put("90th", high);
     floatFeatures.put("DECILES", map);
 
     Map<String, Double> map2 = new HashMap<>();
@@ -48,8 +47,8 @@ public class ApproximatePercentileTransformTest {
            " transform : approximate_percentile\n" +
            " field1 : DECILES\n" +
            " low : 10th\n" +
-           " mid : 50th\n" +
            " upper : 90th\n" +
+           " minDiff : 10 \n" +
            " field2 : F\n" +
            " key2 : foo\n" +
            " output : PERCENTILE\n" +
@@ -72,12 +71,12 @@ public class ApproximatePercentileTransformTest {
     Transform transform = TransformFactory.createTransform(config, "test_approximate_percentile");
 
     double[] values = { -1.0, 10.0, 15.0, 20.0, 50.0, 60.0, 100.0, 200.0 };
-    double[] expected = { 0.0, 0.0, 0.25, 0.5, 0.5 + 0.5 * (30.0 / 80.0), 0.75, 1.0, 1.0 };
+    double[] expected = { 0.0, 0.0, 0.05, 0.11, 0.44, 0.55, 1.0, 1.0 };
 
     for (int i = 0; i < values.length; i++) {
       double val = values[i];
 
-      FeatureVector featureVector = makeFeatureVector(val);
+      FeatureVector featureVector = makeFeatureVector(10.0, 100.0, val);
       transform.doTransform(featureVector);
       Map<String, Set<String>> stringFeatures = featureVector.getStringFeatures();
       assertTrue(stringFeatures.size() == 1);
@@ -86,5 +85,15 @@ public class ApproximatePercentileTransformTest {
       assertTrue(out.size() == 1);
       assertEquals(expected[i], out.get("percentile"), 0.01);
     }
+  }
+
+  @Test
+  public void testAbstain() {
+    Config config = ConfigFactory.parseString(makeConfig());
+    Transform transform = TransformFactory.createTransform(config, "test_approximate_percentile");
+
+    FeatureVector featureVector = makeFeatureVector(10.0, 11.0, 1.0);
+    transform.doTransform(featureVector);
+    assertTrue(featureVector.floatFeatures.get("PERCENTILE") == null);
   }
 }
