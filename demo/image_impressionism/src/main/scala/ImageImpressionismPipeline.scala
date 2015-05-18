fc60ecb679c8883a43df9348918e33cc97152351
Hector Yee <hector.yee@airbnb.com>
2015-05-18 11:36:59 -0700

    Add image reading code

--- demo/image_impressionism/src/main/scala/ImageImpressionismPipeline.scala
+++ demo/image_impressionism/src/main/scala/ImageImpressionismPipeline.scala
@@ -1,5 +1,11 @@
 package com.airbnb.aerosolve.demo.ImageImpressionism;
 
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.Path
 import org.apache.spark.{SparkContext, SparkConf}
 import org.slf4j.{LoggerFactory, Logger}
 import com.typesafe.config.Config
@@ -14,5 +20,10 @@ object ImageImpressionismPipeline {
     val output : String = config.getString("output")
     log.info("Reading image %s".format(imageName))
     log.info("Writing training data to %s".format(output))
+
+    val path = new Path(imageName)
+    val fileSystem = FileSystem.get(new java.net.URI(imageName), new Configuration())
+    val instream = fileSystem.open(path)
+    val image = ImageIO.read(instream)
   }
 }
