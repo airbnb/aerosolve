bc70d5a7500d92df87176afa77c215ff44b74be4
Spike Brehm <ocelot@gmail.com>
2015-06-02 12:11:10 -0700

    Remove code

--- demo/image_impressionism/job_runner.sh
+++
@@ -1,8 +0,0 @@
-spark-submit \
---master local[4] \
---executor-memory 4G \
---driver-memory 4G \
---class com.airbnb.aerosolve.demo.ImageImpressionism.JobRunner \
-build/libs/image_impressionism-0.1.2-all.jar \
-image_impressionism.conf \
-$1
--- demo/income_prediction/job_runner.sh
+++
@@ -1,8 +0,0 @@
-spark-submit \
---master local[4] \
---executor-memory 4G \
---driver-memory 4G \
---class com.airbnb.aerosolve.demo.IncomePrediction.JobRunner \
-build/libs/income_prediction-0.1.3-all.jar \
-income_prediction.conf \
-$1
