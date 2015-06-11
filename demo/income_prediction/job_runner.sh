6b26dfe717cf20838640ffd626aaafdc363a2a9a
Hector Yee <hector.yee@airbnb.com>
2015-06-10 17:55:06 -0700

    Increase all versions to 0.1.7

--- demo/income_prediction/job_runner.sh
+++ demo/income_prediction/job_runner.sh
@@ -3,6 +3,6 @@ spark-submit \
 --executor-memory 4G \
 --driver-memory 4G \
 --class com.airbnb.aerosolve.demo.IncomePrediction.JobRunner \
-build/libs/income_prediction-0.1.6-all.jar \
+build/libs/income_prediction-0.1.7-all.jar \
 income_prediction.conf \
 $1
