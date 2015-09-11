f186424be026346d5107e62e17ef9c3e2f43a3bc
Hector Yee <hector.yee@airbnb.com>
2015-09-11 20:15:10 +0800

    Random forest trainer

---
+++ training/src/main/scala/com/airbnb/aerosolve/training/RandomForestTrainer.scala
@@ -0,0 +1,80 @@
+package com.airbnb.aerosolve.training
+
+import java.util
+
+import com.airbnb.aerosolve.core.models.BoostedStumpsModel
+import com.airbnb.aerosolve.core.models.DecisionTreeModel
+import com.airbnb.aerosolve.core.models.RandomForestModel
+import com.airbnb.aerosolve.core.Example
+import com.airbnb.aerosolve.core.ModelRecord
+import com.airbnb.aerosolve.core.util.Util
+import com.typesafe.config.Config
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd.RDD
+import org.slf4j.{Logger, LoggerFactory}
+
+import scala.util.Random
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+
+// The decision tree is meant to be a prior for the spline model / linear model
+object RandomForestTrainer {
+  private final val log: Logger = LoggerFactory.getLogger("RandomForestTrainer")
+
+  def train(sc : SparkContext,
+            input : RDD[Example],
+            config : Config,
+            key : String) : RandomForestModel = {
+    val candidateSize : Int = config.getInt(key + ".num_candidates")
+    val rankKey : String = config.getString(key + ".rank_key")
+    val rankThreshold : Double = config.getDouble(key + ".rank_threshold")
+    val maxDepth : Int = config.getInt(key + ".max_depth")
+    val minLeafCount : Int = config.getInt(key + ".min_leaf_items")
+    val numTries : Int = config.getInt(key + ".num_tries")
+    
+    val numTrees : Int = config.getInt(key + ".num_trees")
+
+    val examples = LinearRankerUtils
+        .makePointwiseFloat(input, config, key)
+        .map(x => Util.flattenFeature(x.example(0)))
+        .filter(x => x.contains(rankKey))
+        .coalesce(numTrees, true)
+        
+    val trees = examples.mapPartitions(part => {
+      val ex = part
+        .take(candidateSize)
+        .toArray
+      val stumps = new util.ArrayList[ModelRecord]()
+      stumps.append(new ModelRecord)
+      DecisionTreeTrainer.buildTree(
+          stumps,
+          ex,
+          0,
+          0,
+          maxDepth,
+          rankKey,
+          rankThreshold,
+          numTries,
+          minLeafCount)
+      
+      val tree = new DecisionTreeModel()
+      tree.setStumps(stumps)
+  
+      Array(tree).iterator      
+    })
+    .collect
+    
+    val forest = new RandomForestModel()
+    val scale = 1.0f / numTrees.toFloat
+    for (tree <- trees) {
+      for (stump <- tree.getStumps) {
+        if (stump.featureWeight != 0.0f) {
+          stump.featureWeight *= scale
+        }
+      }
+      forest.getTrees().append(tree)
+    }
+    forest
+  }  
+}
