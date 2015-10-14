40e91c6fd51fdad6e79f608d556e51b11c360ecf
Hector Yee <hector.yee@airbnb.com>
2015-10-14 13:55:34 -0700

    More duplicate code removal

--- training/src/test/scala/com/airbnb/aerosolve/training/BoostedStumpsModelTest.scala
+++ training/src/test/scala/com/airbnb/aerosolve/training/BoostedStumpsModelTest.scala
@@ -17,23 +17,6 @@ import scala.collection.mutable.ArrayBuffer
 class BoostedStumpsModelTest {
   val log = LoggerFactory.getLogger("BoostedStumpsModelTest")
 
-  def makeExample(x : Double,
-                  y : Double,
-                  target : Double) : Example = {
-    val example = new Example
-    val item: FeatureVector = new FeatureVector
-    item.setFloatFeatures(new java.util.HashMap)
-    val floatFeatures = item.getFloatFeatures
-    floatFeatures.put("$rank", new java.util.HashMap)
-    floatFeatures.get("$rank").put("", target)
-    floatFeatures.put("loc", new java.util.HashMap)
-    val loc = floatFeatures.get("loc")
-    loc.put("x", x)
-    loc.put("y", y)
-    example.addToExample(item)
-    return example
-  }
-
   def makeConfig(loss : String) : String = {
     """
       |identity_transform {
@@ -70,23 +53,7 @@ class BoostedStumpsModelTest {
   }
 
   def testBoostedStumpTrainer(loss : String) = {
-    val examples = ArrayBuffer[Example]()
-    val label = ArrayBuffer[Double]()
-    val rnd = new java.util.Random(1234)
-    var numPos : Int = 0;
-    for (i <- 0 until 200) {
-      val x = 2.0 * rnd.nextDouble() - 1.0
-      val y = 10.0 * (2.0 * rnd.nextDouble() - 1.0)
-      val poly = x * x + 0.1 * y * y + 0.1 * x + 0.2 * y - 0.1 + Math.sin(x)
-      val rank = if (poly < 1.0) {
-        1.0
-      } else {
-        -1.0
-      }
-      if (rank > 0) numPos = numPos + 1
-      label += rank
-      examples += makeExample(x, y, rank)
-    }
+    val (examples, label, numPos) = TrainingTestHelper.makeClassificationExamples
 
     var sc = new SparkContext("local", "BoostedStumpsTEst")
 
--- training/src/test/scala/com/airbnb/aerosolve/training/MaxoutTrainerTest.scala
+++ training/src/test/scala/com/airbnb/aerosolve/training/MaxoutTrainerTest.scala
@@ -19,23 +19,6 @@ import scala.collection.mutable.ArrayBuffer
 class MaxoutTrainerTest {
   val log = LoggerFactory.getLogger("MaxoutTrainerTest")
 
-  def makeExample(x : Double,
-                  y : Double,
-                  target : Double) : Example = {
-    val example = new Example
-    val item: FeatureVector = new FeatureVector
-    item.setFloatFeatures(new java.util.HashMap)
-    val floatFeatures = item.getFloatFeatures
-    floatFeatures.put("$rank", new java.util.HashMap)
-    floatFeatures.get("$rank").put("", target)
-    floatFeatures.put("loc", new java.util.HashMap)
-    val loc = floatFeatures.get("loc")
-    loc.put("x", x)
-    loc.put("y", y)
-    example.addToExample(item)
-    return example
-  }
-
   def makeConfig(loss : String) : String = {
     """
       |identity_transform {
@@ -75,23 +58,7 @@ class MaxoutTrainerTest {
   }
 
   def testMaxoutTrainer(loss : String) = {
-    val examples = ArrayBuffer[Example]()
-    val label = ArrayBuffer[Double]()
-    val rnd = new java.util.Random(1234)
-    var numPos : Int = 0;
-    for (i <- 0 until 200) {
-      val x = 2.0 * rnd.nextDouble() - 1.0
-      val y = 10.0 * (2.0 * rnd.nextDouble() - 1.0)
-      val poly = x *x + 0.1 * y * y + 0.1 * x + 0.2 * y + 0.3 * x * y - 0.1
-      val rank = if (poly < 1.0) {
-        1.0
-      } else {
-        -1.0
-      }
-      if (rank > 0) numPos = numPos + 1
-      label += rank
-      examples += makeExample(x, y, rank)
-    }
+    val (examples, label, numPos) = TrainingTestHelper.makeClassificationExamples
 
     var sc = new SparkContext("local", "MaxoutTrainerText")
 
@@ -125,16 +92,6 @@ class MaxoutTrainerTest {
       log.info("Num correct = %d, frac correct = %f, num pos = %d, num neg = %d"
                  .format(numCorrect, fracCorrect, numPos, examples.length - numPos))
       assertTrue(fracCorrect > 0.6)
-
-      val inside = makeExample(0, 0.0, 0.0)
-      val builder = new java.lang.StringBuilder();
-      val insideScore = model.debugScoreItem(inside.example.get(0), builder)
-      log.info(builder.toString)
-
-      val outside = makeExample(10.0, 10.0, 0.0)
-      val builder2 = new java.lang.StringBuilder();
-      val outsideScore = model.debugScoreItem(outside.example.get(0), builder2)
-      log.info(builder2.toString)
 
       val swriter = new StringWriter();
       val writer = new BufferedWriter(swriter);
