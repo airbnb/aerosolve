100c339ff598aa12ecb6c1c0f2ed58a874b9f92a
Hector Yee <hector.yee@airbnb.com>
2015-10-14 14:02:38 -0700

    Fix build

---
+++ training/src/test/scala/com/airbnb/aerosolve/training/TrainingTestHelper.scala
@@ -0,0 +1,50 @@
+package com.airbnb.aerosolve.training
+
+import com.airbnb.aerosolve.core.{Example, FeatureVector}
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+import org.junit.Test
+import org.slf4j.LoggerFactory
+import scala.collection.mutable.ArrayBuffer
+
+object TrainingTestHelper {
+  val log = LoggerFactory.getLogger("TrainingTestHelper")
+
+  def makeExample(x : Double,
+                  y : Double,
+                  target : Double) : Example = {
+    val example = new Example
+    val item: FeatureVector = new FeatureVector
+    item.setFloatFeatures(new java.util.HashMap)
+    val floatFeatures = item.getFloatFeatures
+    floatFeatures.put("$rank", new java.util.HashMap)
+    floatFeatures.get("$rank").put("", target)
+    floatFeatures.put("loc", new java.util.HashMap)
+    val loc = floatFeatures.get("loc")
+    loc.put("x", x)
+    loc.put("y", y)
+    example.addToExample(item)
+    return example
+  }
+  
+  def makeClassificationExamples() = {
+    val examples = ArrayBuffer[Example]()
+    val label = ArrayBuffer[Double]()
+    val rnd = new java.util.Random(1234)
+    var numPos : Int = 0;
+    for (i <- 0 until 200) {
+      val x = 2.0 * rnd.nextDouble() - 1.0
+      val y = 10.0 * (2.0 * rnd.nextDouble() - 1.0)
+      val poly = x * x + 0.1 * y * y + 0.1 * x + 0.2 * y - 0.1 + Math.sin(x)
+      val rank = if (poly < 1.0) {
+        1.0
+      } else {
+        -1.0
+      }
+      if (rank > 0) numPos = numPos + 1
+      label += rank
+      examples += makeExample(x, y, rank)
+    }
+    (examples, label, numPos)
+  }
+}
