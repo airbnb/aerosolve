0b626ce64fb88729d870c8f2251682717745b942
julian qian <julian.qian@airbnb.com>
2015-11-04 21:04:47 -0800

    test regression vs classification

--- training/src/test/scala/com/airbnb/aerosolve/training/TrainingTestHelper.scala
+++ training/src/test/scala/com/airbnb/aerosolve/training/TrainingTestHelper.scala
@@ -26,25 +26,62 @@ object TrainingTestHelper {
     example
   }
 
-  def makeClassificationExamples = {
+  private def makeExamples(number : Int, makeLabel : (Double) => Double) = {
     val examples = ArrayBuffer[Example]()
-    val label = ArrayBuffer[Double]()
+    val labels = ArrayBuffer[Double]()
     val rnd = new java.util.Random(1234)
     var numPos : Int = 0
-    for (i <- 0 until 200) {
+    for (i <- 0 until number) {
       val x = 2.0 * rnd.nextDouble() - 1.0
       val y = 10.0 * (2.0 * rnd.nextDouble() - 1.0)
       val poly = x * x + 0.1 * y * y + 0.1 * x + 0.2 * y - 0.1 + Math.sin(x)
-      val rank = if (poly < 1.0) {
-        1.0
-      } else {
-        -1.0
-      }
-      if (rank > 0) numPos = numPos + 1
-      label += rank
-      examples += makeExample(x, y, rank)
+      val label = makeLabel(poly)
+      if (label > 0) numPos = numPos + 1
+      labels += label
+      examples += makeExample(x, y, label)
     }
-    (examples, label, numPos)
+    (examples, labels, numPos)
+  }
+
+  private def reverseBinLabel(label : Double) : Double = {
+    if (label < 1.0) {
+      1.0
+    } else {
+      -1.0
+    }
+  }
+
+  private def binLabel(label : Double) : Double = {
+    if (label >= 0) {
+      1.0
+    } else {
+      -1.0
+    }
+  }
+
+  def makeClassificationExamples = {
+    makeExamples(200, reverseBinLabel)
+  }
+
+  def makeRegressionAndClassificationExamples(number : Int, randomSeed: Int = 1234) = {
+    val cExamples = ArrayBuffer[Example]()
+    val clabels = ArrayBuffer[Double]()
+    val rExamples = ArrayBuffer[Example]()
+    val rlabels = ArrayBuffer[Double]()
+    val rnd = new java.util.Random(1234)
+    var numPos : Int = 0
+    for (i <- 0 until number) {
+      val x = 2.0 * rnd.nextDouble() - 1.0
+      val y = 10.0 * (2.0 * rnd.nextDouble() - 1.0)
+      val poly = x * x + 0.1 * y * y + 0.1 * x + 0.2 * y - 0.1 + Math.sin(x)
+      val label = binLabel(poly)
+      if (label > 0) numPos = numPos + 1
+      clabels += label
+      rlabels += poly
+      cExamples += makeExample(x, y, label)
+      rExamples += makeExample(x, y, poly)
+    }
+    (rExamples, rlabels, cExamples, clabels, numPos)
   }
 
   def makeRegressionExamples(randomSeed: Int = 1234) = {
