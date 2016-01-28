5bc282270de0281469ac42e3df2cdb7d6cf6e309
Hector Yee <hector.yee@airbnb.com>
2016-01-27 16:10:48 -0800

    Normalize float transform

---
+++ core/src/test/java/com/airbnb/aerosolve/core/transforms/NormalizeFloatFeatureTransformTest.java
@@ -0,0 +1,55 @@
+package com.airbnb.aerosolve.core.transforms;
+
+import com.airbnb.aerosolve.core.FeatureVector;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Hector Yee
+ */
+public class NormalizeFloatFeatureTransformTest {
+  private static final Logger log = LoggerFactory.getLogger(CapFloatFeatureTransformTest.class);
+  
+  public String makeConfig() {
+    return "test_norm {\n" +
+           " transform : normalize_float\n" +
+           " field1 : loc\n" +
+           "}";
+  }
+
+  @Test
+  public void testEmptyFeatureVector() {
+    Config config = ConfigFactory.parseString(makeConfig());
+    Transform transform = TransformFactory.createTransform(config, "test_norm");
+    FeatureVector featureVector = new FeatureVector();
+    transform.doTransform(featureVector);
+    assertTrue(featureVector.getStringFeatures() == null);
+  }
+
+  @Test
+  public void testTransform() {
+    Config config = ConfigFactory.parseString(makeConfig());
+    Transform transform = TransformFactory.createTransform(config, "test_norm");
+    FeatureVector featureVector = TransformTestingHelper.makeFeatureVector();
+    transform.doTransform(featureVector);
+    Map<String, Set<String>> stringFeatures = featureVector.getStringFeatures();
+    assertTrue(stringFeatures.size() == 1);
+
+    Map<String, Double> feat1 = featureVector.getFloatFeatures().get("loc");
+
+    assertEquals(3, feat1.size());
+    double scale = 1.0 / Math.sqrt(37.7 * 37.7 + 40.0 * 40.0 + 20.0 * 20.0);
+    assertEquals(scale * 37.7, feat1.get("lat"), 0.1);
+    assertEquals(scale * 40.0, feat1.get("long"), 0.1);
+    assertEquals(scale * -20.0, feat1.get("z"), 0.1);
+  }
+
+}
