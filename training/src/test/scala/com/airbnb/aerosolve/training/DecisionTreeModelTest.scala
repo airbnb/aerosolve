a803dc2072be5587233cb2bc3e94b36c5de3839e
Peng Ye <peng.ye@airbnb.com>
2016-03-15 18:18:46 -0700

    fix test failure

--- training/src/test/scala/com/airbnb/aerosolve/training/DecisionTreeModelTest.scala
+++ training/src/test/scala/com/airbnb/aerosolve/training/DecisionTreeModelTest.scala
@@ -223,7 +223,7 @@ class DecisionTreeModelTest {
       flatRegionExamples.foreach { flatRegionExample =>
         val score = model.scoreItem(flatRegionExample.example.get(0))
 
-        assertEquals(score, -8.0, 1.5f)
+        assertEquals(score, -8.0, 2.0f)
       }
     } finally {
       sc.stop
