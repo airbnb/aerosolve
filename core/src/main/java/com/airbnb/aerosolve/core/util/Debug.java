233b2d60d2936620cedcabad0d2f8cbfcc6c39c5
julian qian <julian.qian@airbnb.com>
2016-03-23 11:41:50 -0700

    add load and save example for debug.

--- core/src/main/java/com/airbnb/aerosolve/core/util/Debug.java
+++ core/src/main/java/com/airbnb/aerosolve/core/util/Debug.java
@@ -1,8 +1,17 @@
 package com.airbnb.aerosolve.core.util;
 
+import com.airbnb.aerosolve.core.Example;
 import com.airbnb.aerosolve.core.FeatureVector;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.thrift.TDeserializer;
+import org.apache.thrift.TSerializer;
+import org.apache.thrift.protocol.TBinaryProtocol;
 
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 import java.util.Set;
 
@@ -91,6 +100,40 @@ public class Debug  {
       if (!a.contains(s)) {
         log.info("a missing {}", s);
       }
+    }
+  }
+
+  /*
+  loadExampleFromResource read example from resources folder, i.e. test/resources
+  use it on unit test to load example from disk
+ */
+  public static Example loadExampleFromResource(String name) {
+    URL url = Debug.class.getResource("/" + name);
+    try {
+      Path path = Paths.get(url.toURI());
+      byte[] bytes = Files.readAllBytes(path);
+      TDeserializer deserializer = new TDeserializer(new TBinaryProtocol.Factory());
+      Example example = new Example();
+      deserializer.deserialize(example, bytes);
+      return example;
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    assert(false);
+    return null;
+  }
+
+  // Save example to path
+  // If you hit permission error, touch and chmod the file
+  public static void saveExample(Example example, String path) {
+    TSerializer serializer = new TSerializer(new TBinaryProtocol.Factory());
+    try {
+      byte[] buf = serializer.serialize(example);
+      FileOutputStream fos = new FileOutputStream(path);
+      fos.write(buf);
+      fos.close();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
   }
 }
