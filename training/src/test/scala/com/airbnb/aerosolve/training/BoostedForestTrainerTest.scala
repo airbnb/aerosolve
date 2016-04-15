79137b230cf03e87af163c904cefd5c253017c54
chao-yang <chao.yang@airbnb.com>
2016-04-14 23:20:31 -0700

    add hinge loss

--- training/src/test/scala/com/airbnb/aerosolve/training/BoostedForestTrainerTest.scala
+++ training/src/test/scala/com/airbnb/aerosolve/training/BoostedForestTrainerTest.scala
@@ -18,7 +18,7 @@ import scala.collection.mutable.ArrayBuffer
 class BoostedForestTrainerTest {
   val log = LoggerFactory.getLogger("BoostedForestTrainerTest")
 
-  def makeConfig(splitCriteria : String, samplingStrategy : String) : String = {
+  def makeConfig(splitCriteria : String, samplingStrategy : String, loss : String) : String = {
     """
       |identity_transform {
       |  transform : list
@@ -28,6 +28,7 @@ class BoostedForestTrainerTest {
       |  rank_key : "$rank"
       |  split_criteria : "%s"
       |  sampling_strategy : "%s"
+      |  loss : "%s"
       |  num_candidates : 1000
       |  rank_threshold : 0.0
       |  max_depth : 3
@@ -42,49 +43,54 @@ class BoostedForestTrainerTest {
       |  combined_transform : identity_transform
       |}
     """.stripMargin
-      .format(splitCriteria, samplingStrategy)
+      .format(splitCriteria, samplingStrategy, loss)
   }
   
   @Test
   def testBoostedForestTrainerHellinger() = {
-    val config = ConfigFactory.parseString(makeConfig("hellinger", "uniform"))
+    val config = ConfigFactory.parseString(makeConfig("hellinger", "uniform", "logistic"))
     ForestTrainerTestHelper.testForestTrainer(config, true, 0.8)
   }
 
   @Test
   def testBoostedForestTrainerMulticlassHellinger() = {
-    val config = ConfigFactory.parseString(makeConfig("multiclass_hellinger", "uniform"))
+    val config = ConfigFactory.parseString(makeConfig("multiclass_hellinger", "uniform", "logistic"))
     ForestTrainerTestHelper.testForestTrainerMulticlass(config, true, 0.8)
   }
 
   @Test
   def testBoostedForestTrainerMulticlassHellingerNonlinear() = {
-    val config = ConfigFactory.parseString(makeConfig("multiclass_hellinger", "uniform"))
+    val config = ConfigFactory.parseString(makeConfig("multiclass_hellinger", "uniform", "logistic"))
     ForestTrainerTestHelper.testForestTrainerMulticlassNonlinear(config, true, 0.6)
   }
 
    @Test
   def testBoostedForestTrainerHellingerSampleFirst() = {
-    val config = ConfigFactory.parseString(makeConfig("hellinger", "first"))
+    val config = ConfigFactory.parseString(makeConfig("hellinger", "first", "logistic"))
     ForestTrainerTestHelper.testForestTrainer(config, true, 0.8)
   }
 
-
   @Test
   def testBoostedForestTrainerGini() = {
-    val config = ConfigFactory.parseString(makeConfig("gini", "uniform"))
+    val config = ConfigFactory.parseString(makeConfig("gini", "uniform", "logistic"))
     ForestTrainerTestHelper.testForestTrainer(config, true, 0.8)
   }
 
   @Test
   def testBoostedForestTrainerMulticlassGiniNonlinear() = {
-    val config = ConfigFactory.parseString(makeConfig("multiclass_gini", "uniform"))
+    val config = ConfigFactory.parseString(makeConfig("multiclass_gini", "uniform", "logistic"))
     ForestTrainerTestHelper.testForestTrainerMulticlassNonlinear(config, true, 0.7)
   }
   
   @Test
   def testBoostedForestTrainerInformationGain() = {
-    val config = ConfigFactory.parseString(makeConfig("information_gain", "uniform"))
+    val config = ConfigFactory.parseString(makeConfig("information_gain", "uniform", "logistic"))
     ForestTrainerTestHelper.testForestTrainer(config, true, 0.8)
-  }  
+  }
+
+  @Test
+  def testBoostedForestTrainerInformationGainHingeLoss() = {
+    val config = ConfigFactory.parseString(makeConfig("information_gain", "uniform", "hinge"))
+    ForestTrainerTestHelper.testForestTrainer(config, true, 0.8)
+  }
 }
