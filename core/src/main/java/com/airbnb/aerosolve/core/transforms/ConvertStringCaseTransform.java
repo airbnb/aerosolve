4e94485e5d1ac36d1a01cb618688f8ed033976f5
christhetree <christhetree@gmail.com>
2016-04-17 23:42:18 -0700

    Make output optional for ConvertStringCaseTransform

--- core/src/main/java/com/airbnb/aerosolve/core/transforms/ConvertStringCaseTransform.java
+++ core/src/main/java/com/airbnb/aerosolve/core/transforms/ConvertStringCaseTransform.java
@@ -4,6 +4,7 @@ import com.airbnb.aerosolve.core.FeatureVector;
 import com.airbnb.aerosolve.core.util.Util;
 import com.typesafe.config.Config;
 
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
@@ -11,6 +12,8 @@ import java.util.Set;
  * Converts strings to either all lowercase or all uppercase
  * "field1" specifies the key of the feature
  * "convert_to_uppercase" converts strings to uppercase if true, otherwise converts to lowercase
+ * "output" optionally specifies the key of the output feature, if it is not given the transform
+ * overwrites / replaces the input feature
  */
 public class ConvertStringCaseTransform implements Transform {
   private String fieldName1;
@@ -21,7 +24,11 @@ public class ConvertStringCaseTransform implements Transform {
   public void configure(Config config, String key) {
     fieldName1 = config.getString(key + ".field1");
     convertToUppercase = config.getBoolean(key + ".convert_to_uppercase");
-    outputName = config.getString(key + ".output");
+    if (config.hasPath(key + ".output")) {
+      outputName = config.getString(key + ".output");
+    } else {
+      outputName = fieldName1;
+    }
   }
 
   @Override
@@ -38,15 +45,23 @@ public class ConvertStringCaseTransform implements Transform {
 
     Set<String> output = Util.getOrCreateStringFeature(outputName, stringFeatures);
 
-    for (String rawString : feature1) {
-      if (rawString == null) continue;
-      String convertedString;
-      if (convertToUppercase) {
-        convertedString = rawString.toUpperCase();
-      } else {
-        convertedString = rawString.toLowerCase();
+    for (Iterator<String> iterator = feature1.iterator(); iterator.hasNext();) {
+      String rawString = iterator.next();
+
+      if (rawString != null) {
+        String convertedString;
+        if (convertToUppercase) {
+          convertedString = rawString.toUpperCase();
+        } else {
+          convertedString = rawString.toLowerCase();
+        }
+        output.add(convertedString);
       }
-      output.add(convertedString);
+
+      // Check reference equality to determine whether the output should overwrite the input
+      if (output == feature1) {
+        iterator.remove();
+      }
     }
   }
 }
