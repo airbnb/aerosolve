5b1c3755bd45c9460ccf62f79948eee24daf9ac5
julian qian <julian.qian@airbnb.com>
2016-04-28 06:13:26 -0700

    Debug returns diff count

--- core/src/main/java/com/airbnb/aerosolve/core/util/Debug.java
+++ core/src/main/java/com/airbnb/aerosolve/core/util/Debug.java
@@ -17,24 +17,27 @@ import java.util.Set;
 
 @Slf4j
 public class Debug  {
-  public static void printDiff(FeatureVector a, FeatureVector b) {
+  public static int printDiff(FeatureVector a, FeatureVector b) {
     final Map<String, Set<String>> stringFeaturesA = a.getStringFeatures();
     final Map<String, Set<String>> stringFeaturesB = b.getStringFeatures();
-    printDiff(stringFeaturesA, stringFeaturesB);
+    int diff = printDiff(stringFeaturesA, stringFeaturesB);
     final Map<String, Map<String, Double>> floatFeaturesA = a.getFloatFeatures();
     final Map<String, Map<String, Double>> floatFeaturesB = b.getFloatFeatures();
-    printFloatDiff(floatFeaturesA, floatFeaturesB);
+    diff += printFloatDiff(floatFeaturesA, floatFeaturesB);
+    return diff;
   }
 
-  private static void printFloatDiff(Map<String, Map<String, Double>> a,
-                                     Map<String, Map<String, Double>> b) {
+  private static int printFloatDiff(Map<String, Map<String, Double>> a,
+                                        Map<String, Map<String, Double>> b) {
+    int diff = 0;
     for (Map.Entry<String, Map<String, Double>> entry : a.entrySet()) {
       String key = entry.getKey();
       Map<String, Double> bSet = b.get(key);
       if (bSet == null) {
         log.info("b miss float family {}", key);
+        diff++;
       } else {
-        printMapDiff(entry.getValue(), bSet);
+        diff += printMapDiff(entry.getValue(), bSet);
       }
     }
 
@@ -43,19 +46,24 @@ public class Debug  {
       Map<String, Double> bSet = a.get(key);
       if (bSet == null) {
         log.info("a miss float family {}", key);
+        diff++;
       }
     }
+    return diff;
   }
 
-  private static void printMapDiff(Map<String, Double> a, Map<String, Double> b) {
+  private static int printMapDiff(Map<String, Double> a, Map<String, Double> b) {
+    int diff = 0;
     for (Map.Entry<String, Double> entry : a.entrySet()) {
       String key = entry.getKey();
       Double bValue = b.get(key);
       if (bValue == null) {
         log.info("b miss feature {} {}", key, entry.getValue());
+        diff++;
       } else {
         if (Math.abs(bValue- entry.getValue()) > 0.01) {
           log.info("feature {} a: {}, b: {}", key, entry.getValue(), bValue);
+          diff++;
         }
       }
     }
@@ -65,19 +73,23 @@ public class Debug  {
       Double bValue = a.get(key);
       if (bValue == null) {
         log.info("a miss feature {} {}", key, entry.getValue());
+        diff++;
       }
     }
+    return diff;
   }
 
 
-  public static void printDiff(Map<String, Set<String>> a, Map<String, Set<String>> b) {
+  public static int printDiff(Map<String, Set<String>> a, Map<String, Set<String>> b) {
+    int diff = 0;
     for (Map.Entry<String, Set<String>> entry : a.entrySet()) {
       String key = entry.getKey();
       Set<String> bSet = b.get(key);
       if (bSet == null) {
         log.info("b miss string family {}", key);
+        diff++;
       } else {
-        printDiff(entry.getValue(), bSet);
+        diff += printDiff(entry.getValue(), bSet);
       }
     }
 
@@ -86,21 +98,27 @@ public class Debug  {
       Set<String> bSet = a.get(key);
       if (bSet == null) {
         log.info("a miss string family {}", key);
+        diff++;
       }
     }
+    return diff;
   }
 
-  private static void printDiff(Set<String> a, Set<String> b) {
+  private static int printDiff(Set<String> a, Set<String> b) {
+    int diff = 0;
     for(String s : a) {
       if (!b.contains(s)) {
         log.info("b missing {}", s);
+        diff++;
       }
     }
     for(String s : b) {
       if (!a.contains(s)) {
         log.info("a missing {}", s);
+        diff++;
       }
     }
+    return diff;
   }
 
   /*
