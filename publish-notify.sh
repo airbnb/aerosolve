f1d5b9fbfb1c8643f52a380f81b8a5e0dada5869
Forest Fang <sen.fang@airbnb.com>
2016-05-02 13:22:27 -0700

    fix quote issue on notification and disable dryrun

--- publish-notify.sh
+++ publish-notify.sh
@@ -4,4 +4,4 @@ author=`git --no-pager show -s --format='%an' ${TRAVIS_COMMIT}`
 text="{\"text\":\"Aerosolve version ${TRAVIS_TAG} has been <https://travis-ci.org/airbnb/aerosolve/builds/${TRAVIS_BUILD_ID}|built> and published by ${author}!\"}"
 
 echo $text
-curl -s -X POST -H 'Content-type: application/json' --data '$text' ${AEROSOLVE_CHANNEL}
+curl -s -X POST -H 'Content-type: application/json' --data "$text" ${AEROSOLVE_CHANNEL}
