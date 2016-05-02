3c31db23eaa27e48220947e549b8798fa5f73c0f
Forest Fang <sen.fang@airbnb.com>
2016-05-02 12:53:25 -0700

    be verbose on publish notification and dryrun on publish for now

--- publish-notify.sh
+++ publish-notify.sh
@@ -3,4 +3,5 @@
 author=`git --no-pager show -s --format='%an' ${TRAVIS_COMMIT}`
 text="{\"text\":\"Aerosolve version ${TRAVIS_TAG} has been <https://travis-ci.org/airbnb/aerosolve/builds/${TRAVIS_BUILD_ID}|built> and published by ${author}!\"}"
 
+echo $text
 curl -s -X POST -H 'Content-type: application/json' --data '$text' ${AEROSOLVE_CHANNEL}
