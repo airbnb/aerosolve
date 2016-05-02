10fd12c563b70224cb873a9d0de6870d484f6e26
christhetree <christhetree@gmail.com>
2016-05-02 03:41:49 -0400

    Add comments to TransformUtil

--- core/src/main/java/com/airbnb/aerosolve/core/util/TransformUtil.java
+++ core/src/main/java/com/airbnb/aerosolve/core/util/TransformUtil.java
@@ -1,7 +1,9 @@
 package com.airbnb.aerosolve.core.util;
 
 /**
- * Created by christhetree on 30/04/16.
+ * Utility class for methods that are commonly used by transforms and should be shared.
+ * TODO: go through all transforms and add all common methods here.
+ * TODO: go through Util class and add all transform related / exclusive methods here.
  */
 public class TransformUtil {
     public static Double quantize(double val, double delta) {
