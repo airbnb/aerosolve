b13cffda919b80a1a28265a4e5a6c12c3127b084
christhetree <christhetree@gmail.com>
2016-05-03 03:30:57 -0400

    Refactor NDTreeTest and add build tree with median test

--- training/src/test/scala/com/airbnb/aerosolve/training/NDTreeTest.scala
+++ training/src/test/scala/com/airbnb/aerosolve/training/NDTreeTest.scala
@@ -12,37 +12,46 @@ class NDTreeTest {
   val log = LoggerFactory.getLogger("NDTreeTest")
 
   @Test
-  def buildTreeTest: Unit = {
-    val pts = ArrayBuffer[Array[Double]]()
-    for (x <- -20 to 20) {
-      for (y <- 1 to 19) {
+  def buildTreeUsingMedianTest: Unit = {
+    val points = ArrayBuffer[Array[Double]]()
+
+    for (x <- -2 to 6) {
+      for (y <- 1 to 41) {
         for (z <- 3 to 18) {
-          pts.append(Array[Double](x.toDouble, y.toDouble, z.toDouble))
+          points.append(Array[Double](x.toDouble, y.toDouble, z.toDouble))
         }
       }
     }
-    val dimensions = pts.head.length
+
+    val dimensions = points.head.length
+
     val options = NDTreeBuildOptions(
       maxTreeDepth = 16,
       minLeafCount = 0,
-      splitType = SplitType.SurfaceArea)
-    val tree = NDTree(options, pts.toArray)
+      splitType = SplitType.Median)
+
+    val tree = NDTree(options, points.toArray)
     val nodes = tree.nodes
-    log.info("Num nodes = %d".format(nodes.size))
-    // Since the x dimension is largest we expect the first node to be an xsplit
+
+    log.info("Number of nodes = %d".format(nodes.length))
+
+    // Since the y dimension is largest, we expect the first node to be a split along the y axis
     assertEquals(0, nodes(0).axisIndex)
-    assertEquals(-1.81, nodes(0).splitValue, 0.1)
+    assertEquals(21.0, nodes(0).splitValue, 0)
     assertEquals(1, nodes(0).leftChild)
-    assertEquals(2, nodes(0).rightChild)    // Ensure every point is bounded in the box of the kdtree
-    for (pt <- pts) {
-      val res = tree.query(pt)
-      for (idx <- res) {
+    assertEquals(2, nodes(0).rightChild)
+
+    // Ensure every point is bounded in the box of the kdtree
+    for (point <- points) {
+      val res = tree.query(point)
+      for (index <- res) {
         for (i <- 0 until dimensions) {
-          assert(pt(i) >= nodes(idx).min.get(i))
-          assert(pt(i) <= nodes(idx).max.get(i))
+          assert(point(i) >= nodes(index).min.get(i))
+          assert(point(i) <= nodes(index).max.get(i))
         }
       }
     }
+
     // Ensure all nodes are sensible
     for (node <- nodes) {
       assert(node.count > 0)
@@ -50,6 +59,63 @@ class NDTreeTest {
       for (i <- 0 until dimensions) {
         assert(node.min.get(i) <= node.max.get(i))
       }
+
+      if (node.axisIndex != NDTreeModel.LEAF) {
+        assert(node.leftChild >= 0 && node.leftChild < nodes.length)
+        assert(node.rightChild >= 0 && node.rightChild < nodes.length)
+      }
+    }
+  }
+
+  @Test
+  def buildTreeUsingSurfaceAreaTest: Unit = {
+    val points = ArrayBuffer[Array[Double]]()
+
+    for (x <- -20 to 20) {
+      for (y <- 1 to 19) {
+        for (z <- 3 to 18) {
+          points.append(Array[Double](x.toDouble, y.toDouble, z.toDouble))
+        }
+      }
+    }
+
+    val dimensions = points.head.length
+
+    val options = NDTreeBuildOptions(
+      maxTreeDepth = 16,
+      minLeafCount = 0,
+      splitType = SplitType.SurfaceArea)
+
+    val tree = NDTree(options, points.toArray)
+    val nodes = tree.nodes
+
+    log.info("Number of nodes = %d".format(nodes.length))
+
+    // Since the x dimension is largest, we expect the first node to be a split along the x axis
+    assertEquals(0, nodes(0).axisIndex)
+    assertEquals(-1.81, nodes(0).splitValue, 0.1)
+    assertEquals(1, nodes(0).leftChild)
+    assertEquals(2, nodes(0).rightChild)
+
+    // Ensure every point is bounded in the box of the kdtree
+    for (point <- points) {
+      val res = tree.query(point)
+      for (index <- res) {
+        for (i <- 0 until dimensions) {
+          assert(point(i) >= nodes(index).min.get(i))
+          assert(point(i) <= nodes(index).max.get(i))
+        }
+      }
+    }
+
+    // Ensure all nodes are sensible
+    for (node <- nodes) {
+      assert(node.count > 0)
+
+      for (i <- 0 until dimensions) {
+        assert(node.min.get(i) <= node.max.get(i))
+      }
+
       if (node.axisIndex != NDTreeModel.LEAF) {
         assert(node.leftChild >= 0 && node.leftChild < nodes.length)
         assert(node.rightChild >= 0 && node.rightChild < nodes.length)
