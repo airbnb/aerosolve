24c23fd5d5628f0170ff2d1dd58cf3f5a88e380a
julian qian <julian.qian@airbnb.com>
2016-05-29 09:32:12 -0700

    Use Slf4j

--- core/src/main/java/com/airbnb/aerosolve/core/models/NDTreeModel.java
+++ core/src/main/java/com/airbnb/aerosolve/core/models/NDTreeModel.java
@@ -7,8 +7,6 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -24,7 +22,6 @@ import java.util.Stack;
 @Slf4j
 public class NDTreeModel implements Serializable {
   private static final long serialVersionUID = -2884260218927875615L;
-  private static final Logger log = LoggerFactory.getLogger(NDTreeModel.class);
   public static final int LEAF = -1;
   private static final double NOVALUE = Double.MIN_VALUE;
   private static final int LEFT = 0;
