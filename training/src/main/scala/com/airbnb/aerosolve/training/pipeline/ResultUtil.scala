de220add3f303ea8298a448f7e13a3e49f2680c3
Andy Baek <andy.baek@airbnb.com>
2016-08-08 23:55:27 -0700

    Format json output with newlines

--- training/src/main/scala/com/airbnb/aerosolve/training/pipeline/ResultUtil.scala
+++ training/src/main/scala/com/airbnb/aerosolve/training/pipeline/ResultUtil.scala
@@ -8,54 +8,54 @@ import scala.collection.mutable
  *
  * Format:
     {
-      "HOLD_PRECISION_RECALL_AUC": 0.897908,
-      "HOLD_ACC": 0.897908,
-      ...
-      "HOLD_THRESHOLDS": [
-        0.021060,
-        0.313343,
-        0.897908,
-        1.190191,
-        1.482474
-      ],
-      "HOLD_RECALLS": [
-        0.021060,
-        0.313343,
-        0.897908,
-        1.190191,
-        1.482474
-      ],
-      "HOLD_PRECISIONS": [
-        0.021060,
-        0.313343,
-        0.897908,
-        1.190191,
-        1.482474
-      ],
-      "TRAIN_PRECISION_RECALL_AUC": 0.897908,
-      "TRAIN_ACC": 0.897908,
-      ...
-      "TRAIN_THRESHOLDS": [
-        0.021060,
-        0.313343,
-        0.897908,
-        1.190191,
-        1.482474
-      ],
-      "TRAIN_RECALLS": [
-        0.021060,
-        0.313343,
-        0.897908,
-        1.190191,
-        1.482474
-      ],
-      "TRAIN_PRECISIONS": [
-        0.021060,
-        0.313343,
-        0.897908,
-        1.190191,
-        1.482474
-      ]
+    "HOLD_PRECISION_RECALL_AUC": 0.897908,
+    "HOLD_ACC": 0.897908,
+    ...
+    "HOLD_THRESHOLDS": [
+      0.021060,
+      0.313343,
+      0.897908,
+      1.190191,
+      1.482474
+    ],
+    "HOLD_RECALLS": [
+      0.021060,
+      0.313343,
+      0.897908,
+      1.190191,
+      1.482474
+    ],
+    "HOLD_PRECISIONS": [
+      0.021060,
+      0.313343,
+      0.897908,
+      1.190191,
+      1.482474
+    ],
+    "TRAIN_PRECISION_RECALL_AUC": 0.897908,
+    "TRAIN_ACC": 0.897908,
+    ...
+    "TRAIN_THRESHOLDS": [
+      0.021060,
+      0.313343,
+      0.897908,
+      1.190191,
+      1.482474
+    ],
+    "TRAIN_RECALLS": [
+      0.021060,
+      0.313343,
+      0.897908,
+      1.190191,
+      1.482474
+    ],
+    "TRAIN_PRECISIONS": [
+      0.021060,
+      0.313343,
+      0.897908,
+      1.190191,
+      1.482474
+    ]
     }
  *
  */
@@ -73,7 +73,9 @@ object ResultUtil {
         name = name.substring(1)
       }
       val value = metrics(i)._2
-      allMetrics.append("\"" + name + "\": " + value)
+      if (!value.isNaN) {
+        allMetrics.append("\"" + name + "\": " + value)
+      }
     }
 
     // Add hold precision recall metrics
@@ -82,13 +84,13 @@ object ResultUtil {
     var holdRecalls = mutable.Buffer[String]()
     for (i <- 0 until holdThresholdPrecisionRecall.length - 1) {
       val elem = holdThresholdPrecisionRecall(i)
-      holdThresholds.append(elem._1.toString)
-      holdPrecisions.append(elem._2.toString)
-      holdRecalls.append(elem._3.toString)
+      holdThresholds.append("\t" + elem._1.toString)
+      holdPrecisions.append("\t" + elem._2.toString)
+      holdRecalls.append("\t" + elem._3.toString)
     }
-    allMetrics.append(holdThresholds.mkString("\"HOLD_THRESHOLDS\": [", ",", "]"))
-    allMetrics.append(holdPrecisions.mkString("\"HOLD_PRECISIONS\": [", ",", "]"))
-    allMetrics.append(holdRecalls.mkString("\"HOLD_RECALLS\": [", ",", "]"))
+    allMetrics.append(holdThresholds.mkString("\"HOLD_THRESHOLDS\": [\n", ",\n", "\n]"))
+    allMetrics.append(holdPrecisions.mkString("\"HOLD_PRECISIONS\": [\n", ",\n", "\n]"))
+    allMetrics.append(holdRecalls.mkString("\"HOLD_RECALLS\": [\n", ",\n", "\n]"))
 
     // Add hold precision recall metrics
     var trainThresholds = mutable.Buffer[String]()
@@ -96,16 +98,16 @@ object ResultUtil {
     var trainRecalls = mutable.Buffer[String]()
     for (i <- 0 until trainThresholdPrecisionRecall.length - 1) {
       val elem = trainThresholdPrecisionRecall(i)
-      trainThresholds.append(elem._1.toString)
-      trainPrecisions.append(elem._2.toString)
-      trainRecalls.append(elem._3.toString)
+      trainThresholds.append("\t" + elem._1.toString)
+      trainPrecisions.append("\t" + elem._2.toString)
+      trainRecalls.append("\t" + elem._3.toString)
     }
-    allMetrics.append(trainThresholds.mkString("\"TRAIN_THRESHOLDS\": [", ",", "]"))
-    allMetrics.append(trainPrecisions.mkString("\"TRAIN_PRECISIONS\": [", ",", "]"))
-    allMetrics.append(trainRecalls.mkString("\"TRAIN_RECALLS\": [", ",", "]"))
+    allMetrics.append(trainThresholds.mkString("\"TRAIN_THRESHOLDS\": [\n", ",\n", "\n]"))
+    allMetrics.append(trainPrecisions.mkString("\"TRAIN_PRECISIONS\": [\n", ",\n", "\n]"))
+    allMetrics.append(trainRecalls.mkString("\"TRAIN_RECALLS\": [\n", ",\n", "\n]"))
 
     // Write to file as a string
-    val json = allMetrics.mkString("{", ",", "}")
+    val json = allMetrics.mkString("{\n", ",\n", "\n}\n")
     PipelineUtil.writeStringToFile(json, resultsOutputPath)
   }
 }
