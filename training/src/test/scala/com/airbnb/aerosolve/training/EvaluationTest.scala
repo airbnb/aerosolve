945f8c85481944813b0186a9782108dcda1f6690
Forest Fang <sen.fang@airbnb.com>
2016-08-25 22:30:15 -0700

    Evaluation on RDD
    Revert code clean up that merges the evaluation of records from RDD and List. Instead of merging the code path using List, perform evaluation on RDD instead to handle large dataset.
    There is no reliable way to get a SparkContext especially with the way we set up our tests. Since there is really no need to run evaluation on List of records and lists can always be parallelize with single function call, we are removing evaluation on list altogether.

--- training/src/test/scala/com/airbnb/aerosolve/training/EvaluationTest.scala
+++ training/src/test/scala/com/airbnb/aerosolve/training/EvaluationTest.scala
@@ -163,34 +163,6 @@ class EvaluationTest {
   }
 
   @Test
-  def evaluationInCorrectListTest(): Unit = {
-    val recs = generateDataPerfect(false).toList
-    var sc = new SparkContext("local", "EvaluationTest")
-    try {
-      val results1 = Evaluation.evaluateBinaryClassification(recs, 11, "!HOLD_F1").toMap
-      val results2 = Evaluation.evaluateBinaryClassification(sc.parallelize(recs), 11, "!HOLD_F1").toMap
-      log.info("Non-RDD eval")
-      results1.foreach(res => log.info("%s = %f".format(res._1, res._2)))
-      log.info("RDD eval")
-      results2.foreach(res => log.info("%s = %f".format(res._1, res._2)))
-
-      assertEquals(results1.getOrElse("!HOLD_AUC", 0.0), results2.getOrElse("!HOLD_ACC", 0.0), 0.1)
-      assertEquals(results1.getOrElse("!HOLD_PRECISION_RECALL_AUC", 0.0), results2.getOrElse("!HOLD_PRECISION_RECALL_AUC", 0.0), 0.1)
-      assertEquals(results1.getOrElse("!HOLD_F1", 0.0), results2.getOrElse("!HOLD_F1", 0.0), 0.1)
-      assertEquals(results1.getOrElse("!HOLD_RECALL", 0.0), results2.getOrElse("!HOLD_RECALL", 0.0), 0.1)
-      assertEquals(results1.getOrElse("!HOLD_PRECISION", 0.0), results2.getOrElse("!HOLD_PRECISION", 0.0), 0.1)
-      assertEquals(results1.getOrElse("!HOLD_RMSE", 0.0), results2.getOrElse("!HOLD_RMSE", 0.0), 0.1)
-      assertEquals(results1.getOrElse("!HOLD_FPR", 0.0), results2.getOrElse("!HOLD_FPR", 0.0), 0.1)
-    } finally {
-      sc.stop
-      sc = null
-      // To avoid Akka rebinding to the same port,
-      // since it doesn't unbind immediately on shutdown
-      System.clearProperty("spark.master.port")
-    }
-  }
-
-  @Test
   def evaluationGaussianTest(): Unit = {
     val recs = generateDataGaussian
     var sc = new SparkContext("local", "EvaluationTest")
@@ -239,27 +211,6 @@ class EvaluationTest {
       // since it doesn't unbind immediately on shutdown
       System.clearProperty("spark.master.port")
     }
-  }
-
-  @Test
-  def evaluationGaussianListTest(): Unit = {
-    val recs = generateDataGaussian.toList
-    val results = Evaluation.evaluateBinaryClassification(recs, 11, "!HOLD_F1").toMap
-    results.foreach(res => log.info("%s = %f".format(res._1, res._2)))
-
-    val THRESHOLD = 0.7
-    assertTrue(results.getOrElse("!TRAIN_ACC", 0.0) > THRESHOLD)
-    assertTrue(results.getOrElse("!TRAIN_AUC", 0.0) > THRESHOLD)
-    assertTrue(results.getOrElse("!TRAIN_PRECISION_RECALL_AUC", 0.0) > THRESHOLD)
-    assertTrue(results.getOrElse("!TRAIN_F1", 0.0) > THRESHOLD)
-    assertTrue(results.getOrElse("!TRAIN_RECALL", 0.0) > THRESHOLD)
-    assertTrue(results.getOrElse("!TRAIN_PRECISION", 0.0) > THRESHOLD)
-    assertTrue(results.getOrElse("!HOLD_ACC", 0.0) > THRESHOLD)
-    assertTrue(results.getOrElse("!HOLD_AUC", 0.0) > THRESHOLD)
-    assertTrue(results.getOrElse("!HOLD_PRECISION_RECALL_AUC", 0.0) > THRESHOLD)
-    assertTrue(results.getOrElse("!HOLD_F1", 0.0) > THRESHOLD)
-    assertTrue(results.getOrElse("!HOLD_RECALL", 0.0) > THRESHOLD)
-    assertTrue(results.getOrElse("!HOLD_PRECISION", 0.0) > THRESHOLD)
   }
 
   @Test
